b65480d771fc01896d19478dbac91c01f2b64671
Geraxxx5 <gerax.pineda@gmail.com>
2025-07-29 16:55:03 -0600

    feat: add socket to sender

--- sender/app.py
+++ sender/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # 00: Hamming
 # 11: CRC
+import socket
+HOST = "127.0.0.1"  
+PORT = 65432 
+
 from presentation import TextFrameEncoder
 from algos.hamming import Hamming
 from algos.crc import CRC
@@ -8,28 +12,44 @@ from link import link
 
 
 encoder = TextFrameEncoder()
-message = input("Bienvenido\nIngrese el mensaje que desea enviar: ")
-message_b = encoder.text_to_binary(message).split(" ")
-
-print(f"\nMensaje Eviado: {message}\nMensaje codificado: {" ".join(message_b)}")
-
-algorithm = ""
-while True:
-    opc = input("\nIngrese que algoritmo desea usar:\n1) Hamming\n2) CRC\nrespuesta: ")
-
-    if(opc == "1"):
-        algorithm = "00"
-        encoder_class = Hamming
-        break
-    elif (opc == "2"):
-        algorithm = "11"
-        encoder_class = CRC  #cambiar
-        break
-
-for i in range(len(message_b)):
-    message_b[i] += algorithm
 
 
-encoded_message = link(encoder_class, " ".join(message_b))
 
-print(f"\nTu mensaje codificado con el algoritmo:\n{encoded_message}")
+print("Emisor (Cliente)")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print(f"Conectado a {HOST}:{PORT}")
+
+    message = input("Bienvenido\nIngrese el mensaje que desea enviar: ")
+    message_b = encoder.text_to_binary(message).split(" ")
+
+    print(f"\nMensaje Eviado: {message}\nMensaje codificado: {" ".join(message_b)}")
+
+    algorithm = ""
+    while True:
+        opc = input("\nIngrese que algoritmo desea usar:\n1) Hamming\n2) CRC\nrespuesta: ")
+
+        if(opc == "1"):
+            algorithm = "00"
+            encoder_class = Hamming
+            break
+        elif (opc == "2"):
+            algorithm = "11"
+            encoder_class = CRC
+            break
+
+    for i in range(len(message_b)):
+        message_b[i] += algorithm
+
+
+    encoded_message = link(encoder_class, " ".join(message_b))
+
+    print(f"\nTu mensaje codificado con el algoritmo:\n{encoded_message}")
+
+    s.sendall(encoded_message.encode('utf-8'))
+
+    print("Mensaje enviado")
+
+    s.close() # Solo lo pongo por si hay que mandar el mensaje y cerrar terminar y ya
+    print("Conexión cerrada")
+
