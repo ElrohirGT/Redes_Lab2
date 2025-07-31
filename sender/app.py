a6e62840db16a6bfcf22fc2d66b12bad71908343
Geraxxx5 <gerax.pineda@gmail.com>
2025-07-30 18:49:35 -0600

    fix: algorithm character position

--- sender/app.py
+++ sender/app.py
@@ -46,7 +46,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     encoded_final = encoded_message.strip(" ").split(" ")
 
     for i in range(len(encoded_final)):
-        encoded_final[i] = algorithm + encoded_final[i].strip(" ")
+        encoded_final[i] =  encoded_final[i].strip(" ") + algorithm
 
 
     messageToSend = " ".join(encoded_final)
